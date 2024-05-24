6d687e01746082199e62362c14087e51fa0638c9
Salih TÜRKOĞLU <77458433+SalihT95@users.noreply.github.com>
2024-05-24 19:37:50 +0300

    proje added

---
+++ app/src/main/java/com/turkoglu/composedeneme/presentation/detail/DetailState.kt
@@ -0,0 +1,17 @@
+package com.turkoglu.composedeneme.presentation.detail
+
+import com.turkoglu.composedeneme.data.remote.dto.Genre
+import com.turkoglu.composedeneme.data.remote.dto.Video
+import com.turkoglu.composedeneme.domain.model.VideoModel
+
+data class DetailState (
+    val genres: List<Genre> = emptyList(),
+    val imdbId: Int = 0,
+    val overview: String="",
+    val popularity: Double=0.0,
+    val posterPath: String ="",
+    val releaseDate: String="",
+    val revenue: Int=0,
+    val title: String="",
+    val voteAverage: Double=0.0
+)
